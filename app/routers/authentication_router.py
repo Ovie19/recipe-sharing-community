3a9d77b5784f15ec58bbc2b4d9c8731e316734ff
destinyrhemabitrus <destinyrhemabitrus@gmail.com>
2026-08-26 07:32:26 +0100

    Add authentication router

--- app/routers/authentication_router.py
+++ app/routers/authentication_router.py
@@ -4,13 +4,13 @@ from app.models.user import UserCreate, LoginRequest
 
 router = APIRouter()
 
-    authentication_service = AuthenticationService()
+authentication_service = AuthenticationService()
 
-    @router.post("/register")
-    def create_user(payload: UserCreate):
-        return authentication_service.create_user(payload)
+@router.post("/register")
+def create_user(payload: UserCreate):
+    return authentication_service.create_user(payload)
 
-    @router.post("/login")
-    def login_user(payload: LoginRequest):
-        return authentication_service.login_user(payload)
+@router.post("/login")
+def login_user(payload: LoginRequest):
+    return authentication_service.login_user(payload)
 
