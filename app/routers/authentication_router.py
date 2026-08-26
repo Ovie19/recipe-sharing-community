1dd170f725ecedc0ff71a6503a968f6b56e133ad
destinyrhemabitrus <destinyrhemabitrus@gmail.com>
2026-08-26 07:27:30 +0100

    Add authentication router

---
+++ app/routers/authentication_router.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+from services.authentication import AuthenticationService
+from app.models.user import UserCreate, LoginRequest
+
+router = APIRouter()
+
+    authentication_service = AuthenticationService()
+
+    @router.post("/register")
+    def create_user(payload: UserCreate):
+        return authentication_service.create_user(payload)
+
+    @router.post("/login")
+    def login_user(payload: LoginRequest):
+        return authentication_service.login_user(payload)
+
